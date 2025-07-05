a298218c32012bd5fd61594ee0b6ff314f21d20d
Alexander Leontyev <orientstar96@gmail.com>
2025-07-05 14:14:51 +0500

    build: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,68 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
-// const ESLintPlugin = require('eslint-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    assetModuleFilename: 'assets/[name][ext]',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new StyleLintPlugin({
-      files: ['**/*.scss'],
-    }),
-    // new ESLintPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+const MODE = {
+  development: 'development',
+  production: 'production',
+};
+
+module.exports = (_env, argv) => {
+  const { mode = MODE.development } = argv;
+  const isDevMode = mode === MODE.development;
+
+  return {
+    mode,
+    devtool: isDevMode && 'source-map',
+    entry: {
+      bundle: path.resolve(__dirname, 'src/index.js'),
     },
-    port: 3000,
-    watchFiles: path.join(__dirname, 'src'),
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
+    output: {
+      filename: 'js/[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
       },
-      {
-        test: [/\.scss$/i, /\.css$/i],
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env']],
+      port: 'auto',
+      watchFiles: path.join(__dirname, 'src'),
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/style.[contenthash].css',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: [/\.css$/i],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(woff|woff2)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]',
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+    optimization: {
+      minimizer: [new CssMinimizerPlugin()],
+    },
+  };
 };
