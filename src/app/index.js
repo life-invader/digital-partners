1f84e08a1ba1d7956d9930a6994ff53fec6dea42
Alexander Leontyev <orientstar96@gmail.com>
2025-07-06 18:11:21 +0500

    fix: minor

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,5 @@
 import { App } from './app';
 import './style/index.css';
 
-/**
- * Функция для запуска приложения
- */
-async function startApp() {
-  const app = new App();
-  await app.init();
-
-  window.calendarApp = app;
-}
-
-// Запуск приложения при загрузке DOM
-document.addEventListener('DOMContentLoaded', startApp);
+const app = new App();
+app.init();
